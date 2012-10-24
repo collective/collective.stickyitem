23c1dbc0662320da86fc50dfc5227b350c811ed9
Roman Kozlovskyi <krzroman@gmail.com>
2012-10-24 15:03:44 +0300

    Updated classifiers

--- setup.py
+++ setup.py
@@ -5,17 +5,29 @@ version = '1.0.4'
 
 setup(name='collective.stickyitem',
       version=version,
-      description="",
+      description="Sticky news items for Plone",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         ],
-      keywords='',
-      author='Roman Kozlovskiy',
+      keywords='pin top plone web sort news',
+      author='Roman Kozlovskyi',
       author_email='support@quintagroup.com',
       url='https://github.com/collective/collective.stickyitem',
       license='GPL',
